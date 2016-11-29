8a393a0a83739d9225ed96b132e1c9e5647be55c
KamaaAn <andruszczak.kamila@gmail.com>
2016-11-29 08:50:18 +0100

    first coment

--- Ex8.5/js/scripts.js
+++ Ex8.5/js/scripts.js
@@ -2,21 +2,14 @@
 
 var a = 1;
 var b = 20;
-var value = (a * a) + (2 * a * b) - (b * b)
+var value = (a * a) + (2 * a * b) - (b * b);
 
 console.log('Wynik (a * a) + (2 * a * b) - (b * b) dla a: ' + a + ' i b: ' + b + ' wynosi: ' + value);
 
-var value = prompt('Wynik (a * a) + (2 * a * b) - (b * b) dla a: ' + a + ' i b: ' + b + ' wynosi: ' + value);
-
-if (value < 0) {
-    console.log('Wynik ujemny');
-} else {
+if (value > 0) {
     console.log('Wynik dodatni');
-}
-
-if (value == 0) {
-    console.log('Rowny 0');
-}
-else {
-    console.log('Rozny od 0');
+} else if (value == 0){
+    console.log('Wynik 0');
+} else {
+    console.log('Wynik ujemny');
 }
--- Ex8.6/js/scripts.js
+++ Ex8.6/js/scripts.js
@@ -1,14 +1,10 @@
 // plik scripts.js
 function getTriangleArea(a, h) {
-var a = 10;
-var h = 20;
+var a = 0;
+var h = 0;
 var triangleArea = a * h / 2;
 console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
 var triangleArea = prompt('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
-}
-
-getTriangleArea();
-
 if ( (a > 0) && (a != 0) ) {
     console.log('OK');
 } else {
@@ -19,3 +15,6 @@ if ( (h > 0) && (h != 0) ) {
 } else {
     console.log('Nieprawidłowe dane');
 }
+}
+
+getTriangleArea();
