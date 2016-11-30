0f90714e1994245872e7aa9a21ce9ce1d559b850
KamaaAn <andruszczak.kamila@gmail.com>
2016-11-30 12:17:06 +0100

    first coment

---
+++ Ex8.9/js/scripts.js
@@ -0,0 +1,11 @@
+// plik scripts.js
+function rysujChoinke(row){
+  for (i=0 ; i < row ; i++){
+      var star = "";
+      for (n =0; n < i*2-1; n++) {
+          star += "*";
+      }
+  console.log(star);
+  }
+};
+rysujChoinke(10);
