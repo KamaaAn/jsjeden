a788df83c6b55317d0f1e5622b2703138e3a217c
KamaaAn <andruszczak.kamila@gmail.com>
2016-12-03 15:08:11 +0100

    first coment

--- Ex8.11/js/scripts.js
+++ Ex8.11/js/scripts.js
@@ -1,7 +1,7 @@
 // plik scripts.js
 
 var list = document.getElementById('Lista');
-var add = document.getElementById('addElem')
+var add = document.getElementById('addElem');
 
 add.addEventListener('click', function() {
     list.innerHTML += '<li>item</li>'
